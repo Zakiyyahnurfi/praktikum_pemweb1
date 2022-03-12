cfcfe110747d5abaab979f8ba666d3b3be9454b3
Zakiyyahnurfi <92964301+Zakiyyahnurfi@users.noreply.github.com>
2022-03-12 19:01:21 +0700

    Add files via upload

---
+++ startbootstrap-sb-admin-gh-pages/view/artikel1.php
@@ -0,0 +1,56 @@
+<?php 
+include '../layout/header.php';
+?>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-8">
+					<h3>
+						STT Terpadu Nurul Fikri
+					</h3>
+					<p>
+					Pada pertengahan Bulan Juli 2020, STT Terpadu Nurul Fikri melaksanakan<strong> Re-Akreditasi Program Studi (Prodi) Teknik Informatika (TI)</strong> tepatnya pada tanggal 14-15 Juli 2020 dilakukan secara daring/online. Tujuan Re-Akreditasi TI ini untuk meningkat penilaian perguruan tinggi terhadap mutu dan kapasitas penyelenggaraan Tridarma perguruan tinggi, untuk menentukan kelayakan program dan satuan pendidikan.</br>
+				</br>STT Terpadu Nurul Fikri telah mendapatkan Surat Keputusan dari BANPT mengenai Peringkat Akreditasi untuk Prodi TI dan meraih predikat Akreditasi “B” (Baik Sekali). Hal ini tertuang dalam SK No. 4151/SK/BAN-PT/Akred/S/VII/2020.</br>
+				</br><small>sumber : https://nurulfikri.ac.id/program-studi-teknik-informatika-di-stt-terpadu-nurul-fikri-sudah-terakreditasi-peringkat-b-baik-sekali/</small>
+					</p>
+				</div>
+				<div class="col-md-4">
+					<form role="form">
+						<div class="form-group">
+							 
+							<label for="exampleInputEmail1">
+								Username Akun
+							</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" />
+						</div>
+						<div class="form-group">
+							 
+							<label for="exampleInputPassword1">
+								Password
+							</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" />
+						</div>
+						<div class="checkbox">
+							 
+							<label>
+								<input type="checkbox" /> Saya bukan robot
+							</label>
+						</div> 
+						<button type="submit" class="btn btn-primary">
+							Submit
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php 
+include '../layout/footer.php';
+?>
