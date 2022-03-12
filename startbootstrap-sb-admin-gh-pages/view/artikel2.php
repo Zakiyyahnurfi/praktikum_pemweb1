beb61aed421077769ffbc04ad5dbb567a26d6b2e
Zakiyyahnurfi <92964301+Zakiyyahnurfi@users.noreply.github.com>
2022-03-12 19:15:18 +0700

    Add files via upload

---
+++ startbootstrap-sb-admin-gh-pages/view/artikel2.php
@@ -0,0 +1,202 @@
+<?php 
+include '../layout/header.php';
+?>
+
+<link href="css2/bootstrap.min.css" rel="stylesheet">
+<link href="css2/style.css" rel="stylesheet">
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				Program Studi Sistem Informasi
+			</h3>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>
+							No.
+						</th>
+						<th>
+							Kode
+						</th>
+						<th>
+							Mata Kuliah
+						</th>
+						<th>
+							SKS
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							1
+						</td>
+						<td>
+                        NF21001
+						</td>
+						<td>
+						Pembentukan Karakter
+						</td>
+						<td>
+							2 (Dua)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							2
+						</td>
+						<td>
+						NF12002
+						</td>
+						<td>
+						Pancasila dan Pendidikan Kewarganegaraan
+						</td>
+						<td>
+                        3 (Tiga)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							3
+						</td>
+						<td>
+						NF12003
+						</td>
+						<td>
+						Bahasa Inggris
+						</td>
+						<td>
+						2 (Dua)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							4
+						</td>
+						<td>
+						IE12001
+						</td>
+						<td>
+						Basis Data I
+						</td>
+						<td>
+						3 (Tiga)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							5
+						</td>
+						<td>
+						IE12003
+						</td>
+						<td>
+						Sistem Operasi
+						</td>
+						<td>
+						2 (Dua)
+						</td>
+					</tr>
+                    <tr>
+						<td>
+							6
+						</td>
+						<td>
+						IE12004
+						</td>
+						<td>
+						Jaringan Komputer
+						</td>
+						<td>
+						3 (Tiga)
+						</td>
+					</tr>
+                    <tr>
+						<td>
+							7
+						</td>
+						<td>
+						IE12005
+						</td>
+						<td>
+						Struktur Data dan Algoritma
+						</td>
+						<td>
+						3 (Tiga)
+						</td>
+					</tr>
+                    <tr>
+						<td>
+							8
+						</td>
+						<td>
+						NF21003
+						</td>
+						<td>
+						Statistik dan Probabilitas
+						</td>
+						<td>
+						3 (Tiga)
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail First" src="https://th.bing.com/th/id/R.ae8db6a335e0299d92f8d8c1412f0f70?rik=GUOtZ2Jt4qw1kg&riu=http%3a%2f%2fwww.scholarsofficial.com%2fwp-content%2fuploads%2f2021%2f03%2fstt-nf.jpg&ehk=M7PtSj4VimfO2kQ1y3vSQWhuIWy%2f55SwA49kWV03MvI%3d&risl=&pid=ImgRaw&r=0">
+						<div class="card-block">
+							<h5 class="card-title">
+								Prodi Bisnis Digital
+							</h5>
+							<p class="card-text">
+                            Prodi yang mempelajari bisnis yang dikembangkan dengan menggunakan teknologi digital.
+                            </p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Second" src="https://1.bp.blogspot.com/-JNlcLvml2yo/XawJ_ptZrwI/AAAAAAAAAFQ/7Eu4BBtl8A8TWOwQeQ2XpCp4QCdROWBrQCEwYBhgL/s1600/88b3960361835836e444d41321daf95e_XL.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+                            Prodi Sistem Informasi
+							</h5>
+							<p class="card-text">
+                            Prodi yang menggabungkan ilmu komputer, bisnis, dan manajemen.
+                            </p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Third" src="https://3.bp.blogspot.com/--yPpo58sdoc/Wr89uqyzq5I/AAAAAAAAC_k/iMaRev7GAPMzeAMKEVu0QC8sEZa14_WFACK4BGAYYCw/s320/STT%2BTerpadu%2BNurul%2BFikri%252C%2BDepok.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+							Prodi Teknik Informatika
+							</h5>
+							<p class="card-text">
+                            Prodi yang mempelajari serta menerapkan prinsip-prinsip ilmu komputer dan analisis matematis dalam perancangan, pengujian, pengembangan, dan evaluasi sistem operasi, perangkat lunak (software), dan kinerja komputer.
+                            </p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php 
+include '../layout/footer.php';
+?>
